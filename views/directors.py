a45fc3425ace78a14401c5c521b96eb14a98a399
Kostyan89 <kostasamin@gmail.com>
2021-12-11 16:03:50 +0200

    add decorators steps 5 and 6 in home task

--- views/directors.py
+++ views/directors.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_restx import Resource, Namespace
 
 from dao.model.director import DirectorSchema
+from helpers import auth_required, admin_required
 from implemented import director_service
 
 director_ns = Namespace('directors')
@@ -9,11 +10,13 @@ director_schema = DirectorSchema()
 
 @director_ns.route('/')
 class DirectorsView(Resource):
+    @auth_required
     def get(self):
         rs = director_service.get_all()
         res = DirectorSchema(many=True).dump(rs)
         return res, 200
 
+    @admin_required
     def post(self):
         req_json = request.json
         new_director = director_service.create(req_json)
@@ -22,16 +25,19 @@ class DirectorsView(Resource):
 
 @director_ns.route('/<int:did>')
 class DirectorView(Resource):
+    @auth_required
     def get(self, did):
         r = director_service.get_one(did)
         sm_d = DirectorSchema().dump(r)
         return sm_d, 200
 
+    @admin_required
     def put(self, did: int):
         req_json = request.json
         updated_director = director_service.filter_by(did).update(req_json)
         return director_schema.dump(updated_director), 204
 
+    @admin_required
     def delete(self, did: int):
         director_service.delete(did)
         return "", 204
--- views/genres.py
+++ views/genres.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_restx import Resource, Namespace
 
 from dao.model.genre import GenreSchema
+from helpers import auth_required, admin_required
 from implemented import genre_service
 
 genre_ns = Namespace('genres')
@@ -9,11 +10,13 @@ genre_schema = GenreSchema()
 
 @genre_ns.route('/')
 class GenresView(Resource):
+    @auth_required
     def get(self):
         rs = genre_service.get_all()
         res = GenreSchema(many=True).dump(rs)
         return res, 200
 
+    @admin_required
     def post(self):
         req_json = request.json
         new_genre = genre_service.create(req_json)
@@ -22,16 +25,19 @@ class GenresView(Resource):
 
 @genre_ns.route('/<int:gid>')
 class GenreView(Resource):
+    @auth_required
     def get(self, gid):
         r = genre_service.get_one(gid)
         sm_d = GenreSchema().dump(r)
         return sm_d, 200
 
+    @admin_required
     def put(self, gid: int):
         req_json = request.json
         updated_genre = genre_service.filter_by(gid).update(req_json)
         return genre_schema.dump(updated_genre), 204
 
+    @admin_required
     def delete(self, gid: int):
         genre_service.delete(gid)
         return "", 204
