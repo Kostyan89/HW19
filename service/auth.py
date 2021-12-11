afc71f20cf46103f041bddda65f75cb1a6977f00
Kostyan89 <kostasamin@gmail.com>
2021-12-11 16:41:53 +0200

    correct auth view and auth_service

---
+++ service/auth.py
@@ -0,0 +1,77 @@
+import calendar
+import datetime
+
+from flask import request
+from flask_restx import abort
+from jwt import jwt
+
+from dao.model.user import User
+from dao.user import UserDAO
+from service.user import UserService
+from setup_db import db
+
+
+secret = 's3cR$eT'
+algo = 'HS256'
+
+
+class AuthService:
+    def create(self):
+        req_json = request.json
+        username = req_json.get("username", None)
+        password = req_json.get("password", None)
+        if None in [username, password]:
+            abort(400)
+
+        user = db.session.query(User).filter(User.username == username).first()
+
+        if user is None:
+            return {"error": "Неверные учётные данные"}, 401
+
+        password_hash = UserService.get_hash(password)
+
+        if password_hash != user.password:
+            return {"error": "Неверные учётные данные"}, 401
+
+        data = {
+            "username": user.username,
+            "role": user.role
+        }
+        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        data["exp"] = calendar.timegm(min30.timetuple())
+        access_token = jwt.encode(data, secret, algorithm=algo)
+        days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
+        data["exp"] = calendar.timegm(days130.timetuple())
+        refresh_token = jwt.encode(data, secret, algorithm=algo)
+        tokens = {"access_token": access_token, "refresh_token": refresh_token}
+
+        return tokens, 201
+
+    def update(self):
+        req_json = request.json
+        refresh_token = req_json.get("refresh_token")
+        if refresh_token is None:
+            abort(400)
+
+        try:
+            data = jwt.decode(jwt=refresh_token, key=secret, algorithms=[algo])
+        except Exception as e:
+            abort(400)
+
+        username = data.get("username")
+
+        user = db.session.query(User).filter(User.username == username).first()
+
+        data = {
+            "username": user.username,
+            "role": user.role
+        }
+        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        data["exp"] = calendar.timegm(min30.timetuple())
+        access_token = jwt.encode(data, secret, algorithm=algo)
+        days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
+        data["exp"] = calendar.timegm(days130.timetuple())
+        refresh_token = jwt.encode(data, secret, algorithm=algo)
+        tokens = {"access_token": access_token, "refresh_token": refresh_token}
+
+        return tokens, 201
--- views/auth.py
+++ views/auth.py
@@ -1,76 +1,21 @@
-import calendar
-import datetime
-import jwt
-from flask import request, abort
+from flask import request
 from flask_restx import Resource, Namespace
-from implemented import user_service
-from dao.model.user import User
-from setup_db import db
+from implemented import auth_service
+
 
 auth_ns = Namespace('auth')
 
 
-secret = 's3cR$eT'
-algo = 'HS256'
+
 
 @auth_ns.route('/')
 class AuthView(Resource):
     def post(self):
         req_json = request.json
-        username = req_json.get("username", None)
-        password = req_json.get("password", None)
-        if None in [username, password]:
-            abort(400)
-
-        user = db.session.query(User).filter(User.username == username).first()
-
-        if user is None:
-            return {"error": "Неверные учётные данные"}, 401
-
-        password_hash = user_service.get_hash(password)
-
-        if password_hash != user.password:
-            return {"error": "Неверные учётные данные"}, 401
-
-        data = {
-            "username": user.username,
-            "role": user.role
-        }
-        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-        data["exp"] = calendar.timegm(min30.timetuple())
-        access_token = jwt.encode(data, secret, algorithm=algo)
-        days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
-        data["exp"] = calendar.timegm(days130.timetuple())
-        refresh_token = jwt.encode(data, secret, algorithm=algo)
-        tokens = {"access_token": access_token, "refresh_token": refresh_token}
-
+        tokens = auth_service.create(req_json)
         return tokens, 201
 
     def put(self):
         req_json = request.json
-        refresh_token = req_json.get("refresh_token")
-        if refresh_token is None:
-            abort(400)
-
-        try:
-            data = jwt.decode(jwt=refresh_token, key=secret, algorithms=[algo])
-        except Exception as e:
-            abort(400)
-
-        username = data.get("username")
-
-        user = db.session.query(User).filter(User.username == username).first()
-
-        data = {
-            "username": user.username,
-            "role": user.role
-        }
-        min30 = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-        data["exp"] = calendar.timegm(min30.timetuple())
-        access_token = jwt.encode(data, secret, algorithm=algo)
-        days130 = datetime.datetime.utcnow() + datetime.timedelta(days=130)
-        data["exp"] = calendar.timegm(days130.timetuple())
-        refresh_token = jwt.encode(data, secret, algorithm=algo)
-        tokens = {"access_token": access_token, "refresh_token": refresh_token}
-
+        tokens = auth_service.update(req_json)
         return tokens, 201
